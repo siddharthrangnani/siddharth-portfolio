8e17d5afa4458bcf93209c640b77a31b6ba3318b
siddharth rangnani <shrangnani@gmail.com>
2026-08-25 16:34:51 +0530

    Added analytics via vercel

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/next"
 import NotFound from "@/pages/not-found";
 import Portfolio from "./pages/portfolio";
 
@@ -23,6 +24,7 @@ function App() {
         <Router />
       </TooltipProvider>
     </QueryClientProvider>
+    <Analytics />
   );
 }
 
